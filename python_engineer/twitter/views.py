951b9f8de87334485f2621145b94cff2cb524bdd
SasaluckTWxMotional <88997564+SasaluckTWxMotional@users.noreply.github.com>
2021-09-19 20:31:15 +0700

    Clean up code: - Reformat file - update requirements.txt

--- python_engineer/twitter/views.py
+++ python_engineer/twitter/views.py
@@ -4,102 +4,95 @@ from . import configurations
 import re
 from dateutil.parser import parse
 
+
 # Endpoint for getting tweets by hashtag.
 @api_view(['GET'])
-def get_tweet_by_hashtag(request,hashtag):
+def get_tweet_by_hashtag(request, hashtag):
+    hashtag = "#" + hashtag.lower() + " -filter:retweets"
 
-	hashtag = "#"+hashtag.lower()+ " -filter:retweets"
+    limit = request.query_params.get('limit', 30)
 
-	limit = request.query_params.get('limit',30)
+    # Calling twitter's API for getting tweets.
+    tweets = configurations.tweepy.Cursor(configurations.api.search,
+                                          q=hashtag,
+                                          lang="en",
+                                          ).items(int(limit))
 
-	# Calling twitter's API for getting tweets.
-	tweets = configurations.tweepy.Cursor(configurations.api.search,
-		q=hashtag,
-		lang="en",
-		).items(int(limit))
+    output = []
+    accounts = {"account": ""}
 
-	output = []
-	accounts = {"account":""}
-	
-	# Generate response
-	for tweet in tweets:
-		temp = {"fullname":"","href":"","id":0,"hashtags":[],"date":"","likes":0,"replies":0,"retweets":0,"text":""}
-		
-		temp["fullname"] = tweet.user.name
-		temp["href"] = "/"+tweet.user.screen_name
-		temp["id"] = tweet.user.id
+    # Generate response
+    for tweet in tweets:
+        temp = {"fullname": "", "href": "", "id": 0, "hashtags": [], "date": "", "likes": 0, "replies": 0,
+                "retweets": 0, "text": ""}
 
-		dt = parse(str(tweet.created_at))
+        temp["fullname"] = tweet.user.name
+        temp["href"] = "/" + tweet.user.screen_name
+        temp["id"] = tweet.user.id
 
-		temp["date"] = str(dt.time().strftime("%I:%M %p")) +" - "+str(dt.date().strftime("%d %b %Y"))
+        dt = parse(str(tweet.created_at))
 
-		temp["likes"] = tweet.favorite_count
-		temp["retweets"] = tweet.retweet_count
-		plain_text = tweet.text
-		temp["text"] = plain_text
-		hashtagList = []
-		split = re.split('(\W)', plain_text)
+        temp["date"] = str(dt.time().strftime("%I:%M %p")) + " - " + str(dt.date().strftime("%d %b %Y"))
 
-		for i in range(0,len(split)):
-	
-		    if split[i] == "#":
-		        hashtagList.append("#"+split[i+1])
-		hashtagList  
-		temp["hashtags"] = hashtagList
- 
-		output.append(temp)
+        temp["likes"] = tweet.favorite_count
+        temp["retweets"] = tweet.retweet_count
+        plain_text = tweet.text
+        temp["text"] = plain_text
+        hashtagList = []
+        split = re.split('(\W)', plain_text)
 
-	accounts["account"]=output
-	return Response(accounts)
+        for i in range(0, len(split)):
+
+            if split[i] == "#":
+                hashtagList.append("#" + split[i + 1])
+
+        temp["hashtags"] = hashtagList
+
+        output.append(temp)
+
+    accounts["account"] = output
+    return Response(accounts)
 
 
 # Endpoint for getting tweets by user.
 @api_view(['GET'])
-def get_user_tweets(request,account_name):
+def get_user_tweets(request, account_name):
+    account_name = "@" + account_name
+    limit = request.query_params.get('limit', 30)
 
-	account_name = "@"+account_name
-	limit = request.query_params.get('limit',30)
+    # Calling twitter's API for getting tweets.
+    tweets = configurations.api.user_timeline(account_name, count=int(limit))
+    output = []
+    accounts = {"account": ""}
 
-	# Calling twitter's API for getting tweets.
-	tweets = configurations.api.user_timeline(account_name,count=int(limit))
-	output = []
-	accounts = {"account":""}
+    # Generate response
+    for tweet in tweets:
+        temp = {"fullname": "", "href": "", "id": 0, "hashtags": [], "date": "", "likes": 0, "replies": 0,
+                "retweets": 0, "text": ""}
 
-	# Generate response
-	for tweet in tweets:
-		temp = {"fullname":"","href":"","id":0,"hashtags":[],"date":"","likes":0,"replies":0,"retweets":0,"text":""}
-		
-		temp["fullname"] = tweet.user.name
-		temp["href"] = "/"+tweet.user.screen_name
-		temp["id"] = tweet.user.id
+        temp["fullname"] = tweet.user.name
+        temp["href"] = "/" + tweet.user.screen_name
+        temp["id"] = tweet.user.id
 
-		dt = parse(str(tweet.created_at))
+        dt = parse(str(tweet.created_at))
 
-		temp["date"] = str(dt.time().strftime("%I:%M %p")) +" - "+str(dt.date().strftime("%d %b %Y"))
+        temp["date"] = str(dt.time().strftime("%I:%M %p")) + " - " + str(dt.date().strftime("%d %b %Y"))
 
-		temp["likes"] = tweet.favorite_count
-		temp["retweets"] = tweet.retweet_count
-		plain_text = tweet.text
-		temp["text"] = plain_text
-		hashtagList = []
-		split = re.split('(\W)', plain_text)
+        temp["likes"] = tweet.favorite_count
+        temp["retweets"] = tweet.retweet_count
+        plain_text = tweet.text
+        temp["text"] = plain_text
+        hashtagList = []
+        split = re.split('(\W)', plain_text)
 
-		for i in range(0,len(split)):
-	
-		    if split[i] == "#":
-		        hashtagList.append("#"+split[i+1])
-		hashtagList  
-		temp["hashtags"] = hashtagList
- 
-		output.append(temp)
+        for i in range(0, len(split)):
 
-	accounts["account"]=output
-	return Response(accounts)
+            if split[i] == "#":
+                hashtagList.append("#" + split[i + 1])
 
+        temp["hashtags"] = hashtagList
 
+        output.append(temp)
 
-
-
-
-
-
+    accounts["account"] = output
+    return Response(accounts)
